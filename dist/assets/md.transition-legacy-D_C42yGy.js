56da306cc4a3ed9af4e970689950fca056909c63
montanogilberto <montanogilberto@gmail.com>
2025-12-12 16:24:28 -0700

    adding style to dashboard

--- dist/assets/md.transition-legacy-D_C42yGy.js
+++ dist/assets/md.transition-legacy-D_C42yGy.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BmlmKFPP.js"],function(n,e){"use strict";var t,i;return{setters:[function(n){t=n.o,i=n.n}],execute:function(){
+System.register(["./index-legacy-DA7xDFeO.js"],function(n,e){"use strict";var t,i;return{setters:[function(n){t=n.o,i=n.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
