3ee71f75644eb6216cabcca762af783aa62f5589
Gilberto Montaño <44681263+montanogilberto@users.noreply.github.com>
2026-09-11 17:14:09 -0700

    Replace rewards tabs with unified dashboard

--- src/pages/clients/RewardsDashboard/RewardsDashboardView.tsx
+++ src/pages/clients/RewardsDashboard/RewardsDashboardView.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { IonPage, IonContent, IonToast, IonLoading, IonSegment, IonSegmentButton, IonLabel } from '@ionic/react';
+import { IonContent, IonLoading, IonPage, IonToast } from '@ionic/react';
 import Header from '../../../components/layout/Header';
 import { usePopovers } from '../../../hooks/usePopovers';
 import { useRewardsDashboard } from './RewardsDashboardLogic';
-import { RewardsDashboardTab } from './RewardsDashboardTypes';
-import RewardsSummaryCards from './components/RewardsSummaryCards';
-import RewardsLedgerList from './components/RewardsLedgerList';
-import RewardsRedemptionsList from './components/RewardsRedemptionsList';
+import RewardsHeroCard from './components/RewardsHeroCard';
+import RewardsTotalsRow from './components/RewardsTotalsRow';
+import RewardsActivityList from './components/RewardsActivityList';
+import RewardsCatalogList from './components/RewardsCatalogList';
 
 const RewardsDashboardView: React.FC = () => {
   const vm = useRewardsDashboard();
@@ -16,19 +16,10 @@ const RewardsDashboardView: React.FC = () => {
     <IonPage>
       <Header screenTitle={`Recompensas · ${vm.clientName}`} showBackButton={true} backButtonHref="/clients" {...pops.headerProps} />
       <IonContent fullscreen className="rewards-dashboard-content">
-        <RewardsSummaryCards vm={vm} />
-
-        <IonSegment
-          value={vm.tab}
-          onIonChange={(e) => vm.setTab(e.detail.value as RewardsDashboardTab)}
-          className="rewards-dashboard-segment"
-        >
-          <IonSegmentButton value="history"><IonLabel>Historial</IonLabel></IonSegmentButton>
-          <IonSegmentButton value="redemptions"><IonLabel>Canjes</IonLabel></IonSegmentButton>
-        </IonSegment>
-
-        {vm.tab === 'history' && <RewardsLedgerList vm={vm} />}
-        {vm.tab === 'redemptions' && <RewardsRedemptionsList vm={vm} />}
+        <RewardsHeroCard vm={vm} />
+        <RewardsTotalsRow vm={vm} />
+        <RewardsActivityList vm={vm} />
+        <RewardsCatalogList vm={vm} />
 
         <IonToast {...vm.toastProps} />
         <IonLoading isOpen={vm.loading} message="Cargando..." />
