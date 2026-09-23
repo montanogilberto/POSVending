6a831748728e14c6a36fcdd7b120e6c6271e7f93
Gilberto Montaño <44681263+montanogilberto@users.noreply.github.com>
2026-09-23 10:15:26 -0700

    feat: add 'factoryRunUsages' to UiFeature type

--- src/config/rolePermissions.ts
+++ src/config/rolePermissions.ts
@@ -81,7 +81,8 @@ export type UiFeature =
   | 'p2pLending'
   | 'game'
   | 'arcade'
-  | 'myRewards';
+  | 'myRewards'
+  | 'factoryRunUsages';
 
 export const ROLE_UI: Record<RoleCode, readonly UiFeature[]> = {
   admin: [
